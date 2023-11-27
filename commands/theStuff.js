0c633cc154cd35e036e8c55770851b6e5626b95c
Your Name <you@example.com>
2023-11-27 11:56:51 +1100

    [ADD] get bot online

---
+++ commands/theStuff.js
@@ -0,0 +1,5 @@
+import { SlashCommandBuilder } from 'discord.js';
+const data = new SlashCommandBuilder()
+  .setName('TheExecute')
+  .setDescription('This is a demo of execute');
+
---
+++ index.js
@@ -0,0 +1,21 @@
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+import { config } from 'dotenv';
+
+config();
+console.log(process.env.CLIENTID);
+console.log(process.env.SERVERID);
+console.log(process.env.TOKEN);
+
+
+//Basic setup to get bot online
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+})
+
+function readyDiscord()
+{
+    console.log("The bot has activated! " + client.user.tag);
+}
+client.login(process.env.TOKEN); // login to discord with token
+client.once(Events.ClientReady, readyDiscord); //turns bot on [indicated by green dot in server]
+
