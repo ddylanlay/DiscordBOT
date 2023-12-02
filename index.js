251b3cd36b0e35d62bebee32e0b3748eda9ab082
Your Name <you@example.com>
2023-12-02 13:20:23 +1100

    [ADD] gitignore for env file

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 import { Client, Events, GatewayIntentBits } from 'discord.js';
 import { config } from 'dotenv';
 import * as theStuff from './commands/thecommands.js';
+import OpenAI from 'openai';
+
+
 config();
 console.log(process.env.CLIENTID);
 console.log(process.env.SERVERID);
 console.log(process.env.TOKEN);
 
 
-//Basic setup to get bot online
+//Basic setup to get bot online and connect to discord API
 const client = new Client({
-    intents: [GatewayIntentBits.Guilds],
+    intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent],
 })
+
+// Connect to OPENAI API
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_KEY}) // This is also the default, can be omitted
+
+//checks when a message is sent on discord
+client.on('messageCreate', async function(message){
+    try{
+        if(message.author.bot) return; // bot does not respond to itself or other bots, only users
+        console.log(message.content);
+        message.reply(`The user said: ${message.content}`)
+    } catch(error){
+        console.log(error);
+    }
+});
 
 function readyDiscord()
 {
